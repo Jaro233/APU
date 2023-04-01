01943d9daa1bb342db74320c31cc05ab8d4605b4
Jaro233 <jaroslaw.waliczek@vp.pl>
2023-04-01 10:52:31 +0200

    zad3

--- zad2/zad2.R
+++ zad2/zad2.R
@@ -1,6 +1,6 @@
 library(ahp)
 library(data.tree)
-setwd("C:/Users/jaro9/Downloads/APU-master/Zad2")
+setwd("C:/Users/jaro9/OneDrive/Desktop/apu/zad2")
 treeAhp <- Load("tree_design.yml")
 
 print(treeAhp, filterFun = isNotLeaf)
